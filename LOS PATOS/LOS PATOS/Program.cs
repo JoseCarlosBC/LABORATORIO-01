5a2080da4b5700ce0aad6be9f5f43dcc55792764
José Carlos Barrios Chitiquez <201800376@colsanjose.edu.gt>
2020-04-27 20:52:44 -0600

    se hizo el titulo

---
+++ LOS PATOS/LOS PATOS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOS_PATOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            encabezados tit = new encabezados();
+            tit.patos();
+
+            Console.ReadKey();
+        }
+
+        class encabezados
+        {
+            public void patos()
+            {
+                Console.WriteLine("  _       _       _\n=(.)__  <(.)__  >(.)__\n (___/   (___/   (___/\n-------LOS PATOS-------");
+            }
+        }
+    }
+}
+
+
